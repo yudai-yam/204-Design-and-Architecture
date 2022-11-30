507f900b8c634926db5594bc24f2637a79aae9bb
Yudai <yudaimadeinjapan@gmail.com>
2022-11-30 16:41:27 +0000

    feeling like comitting

---
+++ Year2/204. Software Design/Coursework/TestInterfaceExtendingAbstractClass.java
@@ -0,0 +1,17 @@
+interface MyAbstractClass
+{
+    public abstract void myAbstractMethod();
+}
+
+interface InterfaceExtendingAbstractClass extends MyAbstractClass
+{
+}
+
+public class  TestInterfaceExtendingAbstractClass{
+    
+public static void main(String[] args) 
+{
+System.out.println("Hello World!");
+}
+}
+
